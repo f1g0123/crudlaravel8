4379e72b2de6eae57fa1c2ecdf84b1f78c301695
f1g0123 <figopratama28@gmail.com>
2023-12-04 10:39:30 +0700

    first commit

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB:: table ('employees') ->insert([
+            'nama' =>  'Figo Pratama',
+            'jeniskelamin' => 'cowo',
+            'notelpon' => '08992560417',
+        ]);
+    }
+}
